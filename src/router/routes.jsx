e0da95a66ca899a18553b456b807ea4df819de42
Shihab Uddin <shihabuddin.dev@gmail.com>
2025-06-28 07:14:04 +0600

    first commit

---
+++ src/router/routes.jsx
@@ -0,0 +1,20 @@
+import {
+    createBrowserRouter,
+} from "react-router";
+import Root from "../layout/Root";
+import Home from "../pages/home/Home";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        Component: Root,
+        children: [
+            {
+                index: true,
+                Component: Home
+            },
+            
+        ]
+    },
+]);
+export default router
