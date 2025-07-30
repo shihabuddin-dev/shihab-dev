7f14f3aaf8bf61f18323148030eb9a700e9db237
Shihab Uddin <shihabuddin.dev@gmail.com>
2025-07-30 21:15:18 +0600

    typewriter added and few updated here

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -30,7 +30,7 @@ const Hero = () => {
                 <h2 className="text-base md:text-2xl font-medium text-base-content/80"
                 >
                     <Typewriter
-                        words={['MERN Stack Developer','Paassionate Full Stack Developer', 'Tech Enthusiast','Learning TypeScript',]}
+                        words={['MERN Stack Developer','Passionate Full Stack Developer', 'Tech Enthusiast','Learning TypeScript',]}
                         loop={100}
                         cursor
                         cursorStyle='|'
